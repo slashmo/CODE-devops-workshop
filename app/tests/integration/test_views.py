24099dda8748037225bd7b80420a904a4fb2e6e5
Moritz Lang <hi@slashmo.codes>
2018-10-19 17:01:11 +0200

    Add integration tests for division

--- app/tests/integration/test_views.py
+++ app/tests/integration/test_views.py
@@ -16,3 +16,16 @@ class ViewTests(TestCase):
     def test_multiply_out_of_bounds(self):
         r = self.client.get("/calc/3*10000")
         self.assertEquals(r.status_code, 403)
+
+    def test_division(self):
+        r = self.client.get("/calc/50/10")
+        self.assertEquals(r.status_code, 200)
+        self.assertEquals(r.body, "5")
+
+    def test_division_out_of_bounds(self):
+        r = self.client.get("/calc/-1001/10")
+        self.assertEquals(r.status_code, 403)
+
+    def test_division_zero(self):
+        r = self.client.get("/calc/100/0")
+        self.assertEquals(r.status_code, 403)
