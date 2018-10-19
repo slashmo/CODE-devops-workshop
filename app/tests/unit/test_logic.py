d11a26b3f03b87bcdba958cf5cab38d42151e5eb
Moritz Lang <hi@slashmo.codes>
2018-10-19 15:15:51 +0200

    Add more tests for multiplication

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -3,9 +3,17 @@ from calculator.logic import Calculator
 
 
 class CalculatorTests(TestCase):
-    def test_mul(self):
+    def test_mul_with_two_positive_numbers(self):
         c = Calculator()
         assert c.mul(5, 10) == 50
+    
+    def test_mul_with_two_negative_numbers(self):
+        c = Calculator()
+        assert c.mul(-5, -10) == 50
+    
+    def test_mul_with_one_negative_one_positive_number(self):
+        c = Calculator()
+        assert c.mul(-5, 10) == -50
 
     def test_div(self):
         pass
